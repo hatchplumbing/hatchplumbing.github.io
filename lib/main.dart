f5a7565003a1c10adfabfc66bbabea9fd6ade696
Your GitHub Username <your-email@example.com>
2026-07-28 21:37:16 -0700

    Set up automated GitHub actions deployment

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:hatch_plumbing_billing/data/notifiers.dart';
 import 'package:hatch_plumbing_billing/views/widget_tree.dart';
 import 'package:hive_flutter/hive_flutter.dart'; // Add this import
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Hive and open a storage box for our ZIP workspace
   await Hive.initFlutter();
-  await Hive.openBox('workspaceBox'); 
-  await Hive.openBox('houseDataBox');  
+  await Hive.openBox('workspaceBox');
+  await Hive.openBox('houseDataBox');
   runApp(const MyApp()); // (Replace with your actual root app widget)
 }
 
@@ -24,18 +23,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-   return ValueListenableBuilder(valueListenable: isDarkModeNotifier, builder: (context, isDarkMode, child) {
-       return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          
-          brightness: isDarkModeNotifier.value ? Brightness.dark : Brightness.light,
-        ),
-      ),
-      home: WidgetTree()
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.amber,
+
+              brightness: isDarkModeNotifier.value
+                  ? Brightness.dark
+                  : Brightness.light,
+            ),
+          ),
+          home: WidgetTree(),
+        );
+      },
     );
-    });
   }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_secure_storage_windows
   syncfusion_pdfviewer_windows
   url_launcher_windows
 )
