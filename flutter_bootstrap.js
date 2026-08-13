a4aa8fd5193dde2dd05cc9dfec3a907e6923c889
hatchplumbing <freddifersmithingston@gmail.com>
2026-08-13 15:58:31 -0700

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1582266418" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1068865224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
